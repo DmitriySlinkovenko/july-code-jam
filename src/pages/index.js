f5a69e794b08b5713b3c32bfc36b0ebc54313bf2
Dmitriy <dmitriy.slinkovenko@gmail.com>
2024-07-12 19:41:50 -0400

    Started functionality development

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,9 @@
 import "../pages/index.css";
+
+const card = document.querySelector(".card");
+
+const cardToggle = () => {
+  card.classList.toggle = "card__animation";
+};
+
+card.addEventListener("click", cardToggle);
