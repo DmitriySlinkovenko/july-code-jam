bbc12383d3bb69d07e387792d495d3e191373dfa
JAK <56848280+jackmalzone@users.noreply.github.com>
2024-07-12 23:18:34 -0700

    add methods to cocktailsApi.js

---
+++ src/utils/cocktailsApi.js
@@ -0,0 +1,93 @@
+const baseUrl = "https://www.thecocktaildb.com/api/json/v2/9973533";
+
+class Api {
+  constructor(options) {
+    this._baseUrl = options.baseUrl;
+    this._headers = options.headers;
+  }
+
+  _checkResponse(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Error: ${res.status}`);
+  }
+
+  async _request(endpoint, options = {}) {
+    const res = await fetch(`${this._baseUrl}${endpoint}`, {
+      ...options,
+      headers: this._headers,
+    });
+    return this._checkResponse(res);
+  }
+
+  async getRecentCocktails() {
+    return this._request("/recent.php");
+  }
+
+  async searchCocktailByName(name) {
+    return this._request(`/search.php?s=${name}`);
+  }
+
+  async listCocktailsByFirstLetter(letter) {
+    return this._request(`/search.php?f=${letter}`);
+  }
+
+  async searchIngredientByName(name) {
+    return this._request(`/search.php?i=${name}`);
+  }
+
+  async lookupCocktailById(id) {
+    return this._request(`/lookup.php?i=${id}`);
+  }
+
+  async lookupIngredientById(id) {
+    return this._request(`/lookup.php?iid=${id}`);
+  }
+
+  async getRandomCocktail() {
+    return this._request(`/random.php`);
+  }
+
+  async searchByIngredient(ingredient) {
+    return this._request(`/filter.php?i=${ingredient}`);
+  }
+
+  async filterByAlcoholic(alcoholic) {
+    return this._request(`/filter.php?a=${alcoholic}`);
+  }
+
+  async filterByCategory(category) {
+    return this._request(`/filter.php?c=${category}`);
+  }
+
+  async filterByGlass(glass) {
+    return this._request(`/filter.php?g=${glass}`);
+  }
+
+  async listCategories() {
+    return this._request(`/list.php?c=list`);
+  }
+
+  async listGlasses() {
+    return this._request(`/list.php?g=list`);
+  }
+
+  async listIngredients() {
+    return this._request(`/list.php?i=list`);
+  }
+
+  async listAlcoholicFilters() {
+    return this._request(`/list.php?a=list`);
+  }
+}
+
+// API INIT
+const cocktailApi = new Api({
+  baseUrl: baseUrl,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default cocktailApi;
